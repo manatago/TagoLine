05c5b00918bd93e35e54a857fa799d5016c3b636
manatago <satoshi@tagomori.biz>
2024-07-21 17:41:04 +0900

    0.1.4

--- lib/TagoLine/LoginButton.rb
+++ lib/TagoLine/LoginButton.rb
@@ -2,7 +2,7 @@ require 'action_view'
 module TagoLine
   class LoginButton
     def self.show()
-      text = this.buttonStyle()
+      text = buttonStyle()
       text+="<a class='line_login_button' href='https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1653867531&redirect_uri=https%3A%2F%2Ftago-line.herokuapp.com%2Fcallback&state=12345abcde&scope=profile%20openid%20email&nonce=09876xyz'>ログイン</a>".html_safe
       text
     end
