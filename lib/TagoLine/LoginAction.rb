33262679001f91acba4bc3d3eff13d8e66dad5aa
manatago <satoshi@tagomori.biz>
2024-07-21 21:45:34 +0900

    0.1.4.15

--- lib/TagoLine/LoginAction.rb
+++ lib/TagoLine/LoginAction.rb
@@ -4,14 +4,32 @@ require 'json'
 
 module TagoLine
   class LoginAction
+
+    def self.loginProcess(controller)
+        if controller.session[:line_sub] && controller.session[:line_sub].length > 0
+            return true
+        end
+
+        if stateIsRight(controller)
+            res = getAccessToken(controller)
+            getUserInfo(controller, res["id_token"])
+            return true
+        else
+            return false
+        end
+    end
+
     def self.stateIsRight(controller)
         controller.session[:line_state] == controller.params[:state]
     end
 
 
-    def self.getUserInfo(id_token)
+    def self.getUserInfo(controller, id_token)
         uri = URI('https://api.line.me/oauth2/v2.1/verify')
         res = Net::HTTP.post_form(uri,'id_token' => id_token , 'client_id' => ENV['LINE_CHANNEL_ID'])
+        controller.session[:line_sub] = JSON.parse(res.body)["sub"]
+        controller.session[:line_name]= JSON.parse(res.body)["name"]
+        controller.session[:line_picture]= JSON.parse(res.body)["picture"]
         JSON.parse(res.body)
     end
 
--- lib/TagoLine/version.rb
+++ lib/TagoLine/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module TagoLine
-  VERSION = "0.1.4.14"
+  VERSION = "0.1.4.15"
   CALLBACK_URL = ENV["LINE_CALLBACK_URL"]
   CHANNEL_ID = ENV["LINE_CHANNEL_ID"]
   CHANNEL_SECRET = ENV["LINE_CHANNEL_SECRET"]
